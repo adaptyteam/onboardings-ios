c831fcca1918c26c98ed4b2a53d0152d16d23727
Aleksei Valiano <vallianos@gmail.com>
2024-07-30 11:41:31 +0200

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "Octoflows",
+
+    platforms: [
+        .iOS("13"),
+    ],
+    products: [
+        .library(
+            name: "Octoflows",
+            targets: ["Octoflows"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Octoflows",
+            dependencies: [],
+            path: "Sources",
+            resources: [.copy("PrivacyInfo.xcprivacy")]
+        ),
+    ]
+)
