955190c38c16ec5e46389cb9fa202fc4e0b770c7
Alexey Goncharov <x401om@gmail.com>
2024-08-06 12:12:58 +0100

    add SwiftUI Demo app

---
+++ Examples/OctoflowsDemo-SwiftUI/OctoflowsDemo-SwiftUI/OctoflowsDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OctoflowsDemo_SwiftUIApp.swift
+//  OctoflowsDemo-SwiftUI
+//
+//  Created by Aleksey Goncharov on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct OctoflowsDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
