2e695e42e14141787f20c72c342b8b31b915dd8b
Aleksei Valiano <vallianos@gmail.com>
2024-08-08 13:43:49 +0200

    refactoring AnalyticsEvent

--- Sources/Event/Parameters/AnalyticsEvent.swift
+++ Sources/Event/Parameters/AnalyticsEvent.swift
@@ -9,28 +9,73 @@
 import Foundation
 
 extension Onbordings {
-    
-    public enum AnalyticsEventName: String, Sendable {
-        case onboardingStarted = "onboarding_started"
-        case screenPresented = "screen_presented"
-        case screenCompleted = "screen_completed"
-        case secondScreenPresented = "second_screen_presented"
-        case registrationScreenPresented = "registration_screen_presented"
-        case productsScreenPresented = "products_screen_presented"
-        case userEmailCollected = "user_email_collected"
-        case onboardingCompleted = "onboarding_completed"
-    }
+    public enum AnalyticsEvent: Sendable, Hashable {
+        case unknown(meta: MetaParameters)
+        case onboardingStarted(meta: MetaParameters)
+        case screenPresented(meta: MetaParameters)
+        case screenCompleted(meta: MetaParameters, formClientId: String?, reply: String?)
+        case secondScreenPresented(meta: MetaParameters)
+        case registrationScreenPresented(meta: MetaParameters)
+        case productsScreenPresented(meta: MetaParameters)
+        case userEmailCollected(meta: MetaParameters)
+        case onboardingCompleted(meta: MetaParameters)
 
-    public struct AnalyticsEvent: Sendable, Hashable {
-        public let name: AnalyticsEventName
-        public let meta: MetaParameters
+        public var meta: MetaParameters {
+            switch self {
+            case let .unknown(meta),
+                 let .onboardingStarted(meta),
+                 let .screenPresented(meta),
+                 let .screenCompleted(meta, _, _),
+                 let .secondScreenPresented(meta),
+                 let .registrationScreenPresented(meta),
+                 let .productsScreenPresented(meta),
+                 let .userEmailCollected(meta),
+                 let .onboardingCompleted(meta):
+                meta
+            }
+        }
 
         init(_ body: BodyDecoder.Dictionary) throws {
-            self.meta = try MetaParameters(body["meta"])
-            guard let name = try AnalyticsEventName(rawValue: body["name"].asString()) else {
-                throw BodyDecoderError.wrongValue
+            let meta = try MetaParameters(body["meta"])
+
+            enum Name: String, Sendable {
+                case onboardingStarted = "onboarding_started"
+                case screenPresented = "screen_presented"
+                case screenCompleted = "screen_completed"
+                case secondScreenPresented = "second_screen_presented"
+                case registrationScreenPresented = "registration_screen_presented"
+                case productsScreenPresented = "products_screen_presented"
+                case userEmailCollected = "user_email_collected"
+                case onboardingCompleted = "onboarding_completed"
             }
-            self.name = name
+
+            let name = try body["name"].asString()
+            let params = try body["params"].asOptionalDictionary()
+
+            self = switch Name(rawValue: name) {
+            case .none:
+                .unknown(meta: meta)
+            case .onboardingStarted:
+                .onboardingStarted(meta: meta)
+            case .screenPresented:
+                .screenPresented(meta: meta)
+            case .screenCompleted:
+                try .screenCompleted(
+                    meta: meta,
+                    formClientId: params?["form_cid"].asOptionalString(),
+                    reply: params?["reply"].asOptionalString()
+                )
+            case .secondScreenPresented:
+                .secondScreenPresented(meta: meta)
+            case .registrationScreenPresented:
+                .registrationScreenPresented(meta: meta)
+            case .productsScreenPresented:
+                .productsScreenPresented(meta: meta)
+            case .userEmailCollected:
+                .userEmailCollected(meta: meta)
+            case .onboardingCompleted:
+                .onboardingCompleted(meta: meta)
+            }
         }
     }
 }
